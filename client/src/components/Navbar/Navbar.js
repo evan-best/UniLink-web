378a3805d2c544a0c1e4bccc9d474ead2da0bae2
evan-best <117332327+evan-best@users.noreply.github.com>
2024-03-03 13:33:33 -0330

    Added styles for navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,25 +4,27 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <nav className="nav" id = "navbar">
-            <div className="nav-content">
-                {/* Logo Here */}
-            </div>
-            <div>
-                <ul>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/getstarted">Get Started</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about">About</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/support">Support</NavLink>
-                    </li>
-                </ul>
+        <nav className="navbar">
+            <div className="container">
+                <div className="logo">
+                    {/* Logo Here */}
+                </div>
+                <div className="nav-elements">
+                    <ul>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/getstarted">Get Started</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about">About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/support">Support</NavLink>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
